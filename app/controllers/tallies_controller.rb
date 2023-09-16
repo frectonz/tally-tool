66e57ad8aee52392672db736a220e5efef287f80
frectonz <fraol0912@gmail.com>
2023-09-16 11:08:11 +0300

    feat: create a tally if it doesn't exist

--- app/controllers/tallies_controller.rb
+++ app/controllers/tallies_controller.rb
@@ -1,6 +1,5 @@
 class TalliesController < ApplicationController
   before_action :set_namespace
-  before_action :set_tally, only: [:show, :update]
   skip_before_action :verify_authenticity_token, only: [:create, :update]
 
   # GET /namespaces/:id/tallies/
@@ -10,6 +9,12 @@ class TalliesController < ApplicationController
   
   # GET /namespaces/:id/tallies/:id
   def show
+    tally = @namespace.tallies.find_by(name: params[:id])
+
+    if !tally
+      render json: { error: 'Tally not found' }, status: :not_found
+    end
+
     render json: @tally, status: :ok
   end
 
@@ -26,21 +31,24 @@ class TalliesController < ApplicationController
 
   # PATCH/PUT /namespaces/:id/tallies/:id?op=(INC|DEC)
   def update
+    tally = @namespace.tallies.find_by(name: params[:id]) || @namespace.tallies.new(name: params[:id])
     operation = params[:op]
 
     case operation
     when 'INC'
-      @tally.count += 1
+      tally.count += 1
     when 'DEC'
-      @tally.count -= 1
+      tally.count -= 1
     else
-      render json: { error: 'Unkown action' }, status: :not_found and return
+      render json: {
+        error: 'The `op` query parameter must be one of the following values: INC, DEC.' 
+      }, status: :bad_request and return
     end
 
-    if @tally.save()
-      render json: @tally, status: :ok
+    if tally.save()
+      render json: tally, status: :ok
     else
-      render json: @tally.errors, status: :unprocessable_entity
+      render json: tally.errors, status: :unprocessable_entity
     end
   end
 
@@ -51,12 +59,5 @@ class TalliesController < ApplicationController
       if !@namespace
         render json: { error: 'Namespace not found' }, status: :not_found
       end
-    end
-  
-    def set_tally
-      @tally = @namespace.tallies.find_by(id: params[:id]) || Tally.find_by(name: params[:id])
-      if !@tally
-        render json: { error: 'Tally not found' }, status: :not_found
-      end
-    end
+    end  
 end
