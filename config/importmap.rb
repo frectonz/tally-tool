f4c25a5a5bf2fe2f1e043d6bff9a21c8c3843492
frectonz <fraol0912@gmail.com>
2023-09-16 12:48:44 +0300

    style: namespace page

---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
