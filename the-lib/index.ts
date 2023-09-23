7f9d74a1740a564142c34a648d99cc5b7659df51
frectonz <fraol0912@gmail.com>
2023-09-23 10:05:42 +0300

    chore: format with prettier

--- the-lib/index.ts
+++ the-lib/index.ts
@@ -1,6 +1,5 @@
 export class TallyTool {
-  constructor(private apiDomain: string) {
-  }
+  constructor(private apiDomain: string) {}
 
   namespace(namespace: string) {
     return new Namespace(this.apiDomain, namespace);
@@ -14,7 +13,7 @@ export type Count = {
   count: number;
   created_at: Date;
   updated_at: Date;
-}
+};
 
 function makeCount(data: any): Count {
   return {
@@ -24,29 +23,36 @@ function makeCount(data: any): Count {
     count: data.count,
     created_at: new Date(data.created_at),
     updated_at: new Date(data.updated_at),
-  }
+  };
 }
 
 export class Namespace {
-  constructor(private apiDomain: string, private namespace: string) {}
+  constructor(
+    private apiDomain: string,
+    private namespace: string,
+  ) {}
 
   count(count: string) {
-    return new Counter(this.apiDomain, this.namespace, count)
+    return new Counter(this.apiDomain, this.namespace, count);
   }
 }
 
 export class Counter {
-  constructor(private apiDomain: string, private namespace: string, private count: string) {}
+  constructor(
+    private apiDomain: string,
+    private namespace: string,
+    private count: string,
+  ) {}
 
   async increment() {
-    const url = `${this.apiDomain}/namespaces/${this.namespace}/tallies/${this.count}?op=INC`
+    const url = `${this.apiDomain}/namespaces/${this.namespace}/tallies/${this.count}?op=INC`;
     const res = await fetch(url, { method: "put" });
     const data = await res.json();
     return makeCount(data);
   }
 
   async decrement() {
-    const url = `${this.apiDomain}/namespaces/${this.namespace}/tallies/${this.count}?op=DEC`
+    const url = `${this.apiDomain}/namespaces/${this.namespace}/tallies/${this.count}?op=DEC`;
     const res = await fetch(url, { method: "put" });
     const data = await res.json();
     return makeCount(data);
