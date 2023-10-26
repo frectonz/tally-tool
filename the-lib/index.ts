70c3d9e4a1a5aa1865281a96996c8973b1e861a6
frectonz <fraol0912@gmail.com>
2023-10-26 15:14:30 +0300

    fix: use headers instead of cookies

--- the-lib/index.ts
+++ the-lib/index.ts
@@ -40,6 +40,7 @@ export class Namespace {
   }
 }
 
+const USER_ACTIONS_HEADER = "X-User-Actions" as const;
 export class Tally {
   constructor(
     private apiDomain: string,
@@ -49,22 +50,59 @@ export class Tally {
 
   async get() {
     const url = `${this.apiDomain}/namespaces/${this.namespace}/tallies/${this.count}`;
-    const res = await fetch(url, { credentials: "include" });
+
+    const actions = getUserActions(this.namespace, this.count);
+    const res = await fetch(url, {
+      headers: { [USER_ACTIONS_HEADER]: actions.toString() },
+    });
+
     const data = await res.json();
     return makeCount(data);
   }
 
   async increment() {
     const url = `${this.apiDomain}/namespaces/${this.namespace}/tallies/${this.count}?op=INC`;
-    const res = await fetch(url, { method: "put", credentials: "include" });
+
+    const actions = getUserActions(this.namespace, this.count);
+    const res = await fetch(url, {
+      method: "put",
+      headers: { [USER_ACTIONS_HEADER]: actions.toString() },
+    });
+    incUserActions(this.namespace, this.count);
+
     const data = await res.json();
     return makeCount(data);
   }
 
   async decrement() {
     const url = `${this.apiDomain}/namespaces/${this.namespace}/tallies/${this.count}?op=DEC`;
-    const res = await fetch(url, { method: "put", credentials: "include" });
+
+    const actions = getUserActions(this.namespace, this.count);
+    const res = await fetch(url, {
+      method: "put",
+      headers: { [USER_ACTIONS_HEADER]: actions.toString() },
+    });
+    decUserActions(this.namespace, this.count);
+
     const data = await res.json();
     return makeCount(data);
   }
+}
+
+function userActionsKey(namespace: string, count: string) {
+  return `${namespace}:${count}:actions`;
+}
+
+function getUserActions(namespace: string, count: string) {
+  return parseInt(localStorage.getItem(userActionsKey(namespace, count))!) || 0;
+}
+
+function incUserActions(namespace: string, count: string) {
+  const actions = getUserActions(namespace, count) + 1;
+  localStorage.setItem(userActionsKey(namespace, count), actions.toString());
+}
+
+function decUserActions(namespace: string, count: string) {
+  const actions = getUserActions(namespace, count) - 1;
+  localStorage.setItem(userActionsKey(namespace, count), actions.toString());
 }
